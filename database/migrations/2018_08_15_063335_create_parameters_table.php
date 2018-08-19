d78cccb0c693a5d0a422a9e0cf7cb654c4c89cec
mian722 <40458531+mian722@users.noreply.github.com>
2018-08-19 12:52:57 +0500

    update

--- database/migrations/2018_08_15_063335_create_parameters_table.php
+++ database/migrations/2018_08_15_063335_create_parameters_table.php
@@ -15,15 +15,15 @@ class CreateParametersTable extends Migration
     {
         Schema::create('parameters', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('sub_id', 30)->nullable();
-            $table->Integer('adv_sub1', 30)->nullable();
-            $table->Integer('adv_sub2', 30)->nullable();
-            $table->Integer('adv_sub3', 30)->nullable();
-            $table->Integer('aff_sub1', 30)->nullable();
-            $table->Integer('aff_sub2', 30)->nullable();
-            $table->Integer('aff_sub3', 30)->nullable();
-            $table->Integer('aff_sub4', 30)->nullable();
-            $table->Integer('aff_sub5', 30)->nullable();
+            $table->string('sub_id')->nullable();
+            $table->Integer('adv_sub1')->nullable();
+            $table->Integer('adv_sub2')->nullable();
+            $table->Integer('adv_sub3')->nullable();
+            $table->Integer('aff_sub1')->nullable();
+            $table->Integer('aff_sub2')->nullable();
+            $table->Integer('aff_sub3')->nullable();
+            $table->Integer('aff_sub4')->nullable();
+            $table->Integer('aff_sub5')->nullable();
             $table->Integer('unique_click')->default(1);
             $table->Integer('conversion_ip')->default(0);
             $table->timestamps();
