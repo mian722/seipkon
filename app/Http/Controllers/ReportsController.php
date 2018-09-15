96f70a9e2d090716e37a335da4152f885224a7b5
Mohsin Javeid <mohsys768@gmail.com>
2018-09-15 11:20:41 +0500

    update

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -28,24 +28,29 @@ class ReportsController extends Controller
 
       $status1 = '';
       $status2 = '';
+      $status3 = '';
       if(isset($data->conversion_status) && $data->conversion_status != 'null'){
           $status1 = " and I1.status = ".$data->conversion_status;
-          $status2 = " and I2.status = ".$data->conversion_status;
+          $status2 = " and uc.status = ".$data->conversion_status;
+          $status3 = " and I2.status = ".$data->conversion_status;
       }
       $range1 = '';
       $range2 = '';
+      $range3 = '';
       if(isset($data->daterange)){
           $explode = explode(" - ", $data->daterange);
           $startdate = $explode[0].' 00:00:00';
           $enddate = $explode[1].' 23:59:59';
           $range1 = " and I1.updated_at BETWEEN '".$startdate."' AND '".$enddate."'";
-          $range2 = " and I2.updated_at BETWEEN '".$startdate."' AND '".$enddate."'";
+          $range2 = " and uc.updated_at BETWEEN '".$startdate."' AND '".$enddate."'";
+          $range3 = " and I2.updated_at BETWEEN '".$startdate."' AND '".$enddate."'";
       }
 
 
       $query = "SELECT  u.*, a.*
       , (SELECT COUNT(I1.click) FROM clicks I1 WHERE I1.offer_id = a.id ".$status1.$range1.") as sumclicks
-      , (SELECT COUNT(I2.signup) FROM signups I2 WHERE I2.offer_id = a.id ".$status2.$range2.") as sumsignup
+      , (SELECT COUNT(DISTINCT uc.ip) FROM clicks uc WHERE uc.offer_id = a.id ".$status2.$range2.") as uniquesumclicks
+      , (SELECT COUNT(I2.signup) FROM signups I2 WHERE I2.offer_id = a.id ".$status3.$range3.") as sumsignup
       FROM users u LEFT JOIN offers a
          ON u.id= a.adv_id
          WHERE u.roles_id = 4 and u.admin_id = ".Auth::user()->id;
@@ -75,43 +80,7 @@ class ReportsController extends Controller
       }
       $query .= " ORDER BY a.id ASC";
 
-      //return $query;
       $alldata = DB::select($query);
-
-
-      // $adv = User::find(2);
-      // return $adv->findoffers(1)->get();
-
-
-
-
-      // $alldata = User::Join('offers', 'offers.adv_id', '=', 'users.id')
-      // ->Join('offer_restrictions', 'offer_restrictions.offer_id', '=', 'offers.id');
-      // (isset($data->advertiser_id) ? (is_array($data->advertiser_id)) ? $alldata->whereIn('users.id',$data->advertiser_id) : $alldata->where('users.id',$data->advertiser_id) : '');
-      // (isset($data->adv_manager_value) ? (is_array($data->adv_manager_value)) ? $alldata->whereIn('users.managerid',$data->adv_manager_value) : $alldata->where('users.managerid',$data->adv_manager_value) : '');
-      // (isset($data->offers_id) ? (is_array($data->offers_id)) ? $alldata->whereIn('offers.id',$data->offers_id) : $alldata->where('offers.id',$data->offers_id) : '');
-      // (isset($data->conversion_status) ? (is_array($data->conversion_status)) ? $alldata->whereIn('users.status',$data->conversion_status) : $alldata->where('users.status',$data->conversion_status) : '');
-      // ((isset($data->timezone) && !empty($data->timezone)) ? $alldata->where('offer_restrictions.caps_timezone',$data->timezone) : '');
-      // $alldata->select('users.*', 'offers.*', 'offer_restrictions.caps_timezone');
-      //return $alldata->get();
-      // if (isset($data->daterange)) {
-      //   $data->->whereBetween('clicks.updated_at', [
-      //                       Carbon\Carbon::parse('today')->startOfDay(),
-      //                       Carbon\Carbon::parse('today')->endOfDay(),
-      //                       ]);
-      // }
-
-
-      //           for($i = 0; $i <= count($alldata); $i++) {
-      //             echo $alldata[$i];
-      //           }
-      // return App\Clicks::where('uid',$offer->uid)->where('oid',$offer->oid)->select(DB::raw("IFNULL(sum(clicks.click),0) as click"))->whereBetween('clicks.updated_at', [
-      //                       Carbon\Carbon::parse('today')->startOfDay(),
-      //                       Carbon\Carbon::parse('today')->endOfDay(),
-      //                       ])->get();
-
-      // return $postbacks = User::Join('clicks', 'clicks.affiliate_id', '=', 'users.id')
-      //                   ->get();
 
     	$table = '<table id="button_datatables_example" class="table display table-striped table-bordered">
                             <thead>
@@ -151,7 +120,7 @@ class ReportsController extends Controller
                                    '.((isset($data->adv_manager)) ? '<td><a href="'.route('affiliate.show', $value->managerid).'">'.(!empty($managername->fname) ? $managername->fname : "" ).'</a></td>' : '').'
                                    '.((isset($data->offer)) ? '<td>'.$value->offer_name.'</td>' : '').'
                                    '.((isset($data->clicks)) ? '<td>'.$value->sumclicks.'</td>' : '').'
-                                   '.((isset($data->unique_clicks)) ? '<td>'.$value->sumclicks.'</td>' : '').'
+                                   '.((isset($data->unique_clicks)) ? '<td>'.$value->uniquesumclicks.'</td>' : '').'
                                    '.((isset($data->currency)) ? '<td>USD</td>' : '').'
                                    '.((isset($data->revenue)) ? '<td>$'.$value->revenue.' ('.$value->revenue_type.')</td>' : '').'
                                    '.((isset($data->conversions)) ? '<td>'.$value->sumsignup.'</td>' : '').'
